c0aa465a8eeaf368b77543e49578b3e828b60177
RomuloAraujo <cromuloaraujo@gmail.com>
2023-01-27 17:06:40 -0300

    A url está /api/generos
    A url está /api/genero para A url está /api/generos

--- src/main/java/com/capgemini/start/api/resource/impl/GeneroResourceImpl.java
+++ src/main/java/com/capgemini/start/api/resource/impl/GeneroResourceImpl.java
@@ -26,7 +26,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/api/genero")
+@RequestMapping(value = "/api/generos")
 public class GeneroResourceImpl implements GeneroResource{
 	
 	private final GeneroService service;
